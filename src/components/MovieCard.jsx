befacfa60acb37ac5fe3c41da2ebea600a801cbb
arriazu.138772 <arriazu.138772@e.unavarra.es>
2026-03-14 18:09:13 +0100

    feat: Configuración de la pantalla de detalles

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,21 @@
+import { Link } from 'react-router'
+
 function MovieCard({ movie }) {
   return (
     <article className="movie-card">
-      <img
-        src={movie.image}
-        alt={movie.title}
-        className="movie-poster"
-      />
+      <Link to={`/movie/${movie.id}`}>
+        <img
+          src={movie.image}
+          alt={movie.title}
+          className="movie-poster"
+        />
 
-      <div className="movie-card-body">
-        <h3>{movie.title}</h3>
-        <p><strong>Categoría:</strong> {movie.category}</p>
-        <p><strong>Año:</strong> {movie.year}</p>
-      </div>
+        <div className="movie-card-body">
+          <h3>{movie.title}</h3>
+          <p><strong>Categoría:</strong> {movie.category}</p>
+          <p><strong>Año:</strong> {movie.year}</p>
+        </div>
+      </Link>
     </article>
   )
 }
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,5 +1,32 @@
+import { useParams } from 'react-router'
+import { movies } from '../data/movies'
+
 function MovieDetail() {
-  return <h1>Detalle de película</h1>
+  const { id } = useParams()
+
+  const movie = movies.find((movie) => movie.id === Number(id))
+
+  if (!movie) {
+    return <h2>Película no encontrada</h2>
+  }
+
+  return (
+    <section className="movie-detail">
+      <h2>{movie.title}</h2>
+
+      <img
+        src={movie.image}
+        alt={movie.title}
+        className="movie-detail-poster"
+      />
+
+      <p><strong>Categoría:</strong> {movie.category}</p>
+      <p><strong>Año:</strong> {movie.year}</p>
+      <p><strong>Director:</strong> {movie.director}</p>
+      <p><strong>Duración:</strong> {movie.duration} minutos</p>
+      <p><strong>Descripción:</strong> {movie.description}</p>
+    </section>
+  )
 }
 
 export default MovieDetail
